516ab3c48fdf72e98dc5df0ddb6c533a58cb99f8
Michael Vogl <info@mv-web.net>
2017-09-19 11:40:31 +0200

    improve code style

--- api.rb
+++ api.rb
@@ -10,15 +10,15 @@ class GivveApiAccess
   end
 
   def get_vouchers(id=nil)
-    return do_get('vouchers/' + (id || ''))
+    do_get('vouchers/' + (id || ''))
   end
 
   def get_vouchers_transactions(id)
-    return do_get('vouchers/' + id + '/transactions')
+    do_get('vouchers/' + id + '/transactions')
   end
 
   def update_customer(voucher_id, id, first_name, last_name)
-    return do_post('vouchers/' + voucher_id + '/owners/' + id,
+    do_post('vouchers/' + voucher_id + '/owners/' + id,
       {'first_name': first_name, 'last_name': last_name},
       true)
   end
@@ -30,13 +30,13 @@ class GivveApiAccess
       request = Net::HTTP::Post.new(url)
     else
       request = Net::HTTP::Put.new(url)
-      request["accept-version"] = 'v2'
-      request["authorization"] = @token
+      request['accept-version'] = 'v2'
+      request['authorization'] = @token
 
     end
     request.set_form_data(data)
 
-    return res = Net::HTTP.start(url.hostname, url.port,
+    res = Net::HTTP.start(url.hostname, url.port,
       :use_ssl => url.scheme == 'https') {|http|
       http.request(request).body
     }
@@ -46,10 +46,10 @@ class GivveApiAccess
     url = URI('https://www.givve.com/api/' + path)
     request = Net::HTTP::Get.new(url)
 
-    request["accept-version"] = 'v2'
-    request["authorization"] = @token
+    request['accept-version'] = 'v2'
+    request['authorization'] = @token
 
-    return res = Net::HTTP.start(url.hostname, url.port,
+    res = Net::HTTP.start(url.hostname, url.port,
       :use_ssl => url.scheme == 'https') {|http|
       JSON.parse(http.request(request).body)['data']
     }
