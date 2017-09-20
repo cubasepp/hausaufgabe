77fad3f0f40ab77b1eaab4f2a7b4445f01e03744
Michael Vogl <info@mv-web.net>
2017-09-20 14:18:51 +0200

    Refeactor api class

--- api.rb
+++ api.rb
@@ -6,52 +6,51 @@ class GivveApiAccess
   @token = nil
 
   def initialize(token)
-    @token = do_post('sessions/', {'token': token})
+    data = {
+      'token': token
+    }
+    @token = do_request('sessions/', 'POST', data)
   end
 
-  def get_vouchers(id=nil)
-    do_get('vouchers/' + (id || ''))
+  def get_vouchers(id='')
+   res = do_request('vouchers/' + id)
+   JSON.parse(res)['data']
   end
 
   def get_vouchers_transactions(id)
-    do_get('vouchers/' + id + '/transactions')
+    res = do_request('vouchers/' + id + '/transactions')
+    JSON.parse(res)['data']
   end
 
   def update_customer(voucher_id, id, first_name, last_name)
-    do_post('vouchers/' + voucher_id + '/owners/' + id,
-      {'first_name': first_name, 'last_name': last_name},
-      true)
+    data = {
+      'first_name': first_name,
+      'last_name': last_name
+    }
+    res = do_request('vouchers/' + voucher_id + '/owners/' + id, 'PUT', data)
+    JSON.parse(res)['data']
   end
 
   private
-  def do_post(path, data, put=nil)
+
+  def do_request(path, action='GET', params=nil)
     url = URI('https://www.givve.com/api/' + path)
-    if put.nil?
+
+    if action.eql?('POST')
       request = Net::HTTP::Post.new(url)
     else
-      request = Net::HTTP::Put.new(url)
+      request = action.eql?('PUT') ? Net::HTTP::Put.new(url) : Net::HTTP::Get.new(url)
       request['accept-version'] = 'v2'
       request['authorization'] = @token
-
     end
-    request.set_form_data(data)
+
+    if not params.nil?
+      request.set_form_data(params)
+    end
 
     res = Net::HTTP.start(url.hostname, url.port,
-      :use_ssl => url.scheme == 'https') {|http|
+                          use_ssl: url.scheme == 'https') { |http|
       http.request(request).body
-    }
-  end
-
-  def do_get(path)
-    url = URI('https://www.givve.com/api/' + path)
-    request = Net::HTTP::Get.new(url)
-
-    request['accept-version'] = 'v2'
-    request['authorization'] = @token
-
-    res = Net::HTTP.start(url.hostname, url.port,
-      :use_ssl => url.scheme == 'https') {|http|
-      JSON.parse(http.request(request).body)['data']
     }
   end
 end
